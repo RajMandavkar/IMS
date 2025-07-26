6e09f7c8308fb2378ade31ecfa20406e8adf4aed
Raj <mandavkarraj7@gmail.com>
2025-07-26 18:14:59 +0530

    logout functionality created

--- src/app/User/user-dashboard/user-dashboard.ts
+++ src/app/User/user-dashboard/user-dashboard.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [],
   templateUrl: './user-dashboard.html',
-  styleUrl: './user-dashboard.css'
+  styleUrls: ['./user-dashboard.css']
 })
 export class UserDashboard {
+  constructor(private router: Router) {}
 
+  logout() {
+    debugger
+    localStorage.removeItem('isLoggedIn');
+    Swal.fire("Success", "Successfylly Logged Out, Please Relogin for Access)", "success");
+    this.router.navigate(['/userLogin']);
+  }
 }
+
